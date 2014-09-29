b603ec8e98c34036377e4af526e80fa06132fe92
Luke Melia <luke@lukemelia.com>
2014-09-29 14:45:20 -0400

    Add an enabled option (defaulting to true) to allow the add-on to be turned on and off (based on the environment, for example).

--- index.js
+++ index.js
@@ -8,16 +8,20 @@ function EmberCLIBase64CSS(project) {
 }
 
 EmberCLIBase64CSS.prototype.postprocessTree = function (type, tree) {
-  if (type === 'all' || type === 'styles') {
-    tree = base64CSS(tree, this.options);
+  if (this.options.enabled) {
+    if (type === 'all' || type === 'styles') {
+      tree = base64CSS(tree, this.options);
+    }
   }
-
   return tree;
 };
 
 EmberCLIBase64CSS.prototype.included = function included(app) {
   this.app = app;
   this.options = this.app.options.base64CSS;
+  if (this.options.enabled === undefined) {
+    this.options.enabled = true;
+  }
 };
 
 EmberCLIBase64CSS.prototype.treeFor = function treeFor() {};
