f44bb1e07bad96657aad6fbb7fff1764d8708b15
Luke Melia <luke@lukemelia.com>
2014-09-29 15:46:05 -0400

    Update to new format for add-on definition.

--- index.js
+++ index.js
@@ -1,25 +1,21 @@
 'use strict';
-
 var base64CSS = require('broccoli-base64-css');
 
-function EmberCLIBase64CSS(project) {
-  this.project = project;
-  this.name = 'Ember CLI Base64CSS';
-}
-
-EmberCLIBase64CSS.prototype.postprocessTree = function (type, tree) {
-  if (type === 'all' || type === 'styles') {
-    tree = base64CSS(tree, this.options);
+module.exports = {
+  name: 'ember-cli-base64-css',
+  postprocessTree: function (type, tree) {
+    if (this.options.enabled) {
+      if (type === 'all' || type === 'styles') {
+        tree = base64CSS(tree, this.options);
+      }
+    }
+    return tree;
+  },
+  included: function included(app) {
+    this.app = app;
+    this.options = this.app.options.base64CSS;
+    if (this.options.enabled === undefined) {
+      this.options.enabled = true;
+    }
   }
-
-  return tree;
-};
-
-EmberCLIBase64CSS.prototype.included = function included(app) {
-  this.app = app;
-  this.options = this.app.options.base64CSS;
-};
-
-EmberCLIBase64CSS.prototype.treeFor = function treeFor() {};
-
-module.exports = EmberCLIBase64CSS;
+}
